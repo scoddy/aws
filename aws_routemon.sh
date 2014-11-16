a3507bf5a99682d358d14d4d0b91442558fb05b8
Florian Daniel <admin@ip-10-142-31-7.eu-central-1.compute.internal>
2014-11-16 18:42:37 +0000

    Corrected SA

--- aws_routemon.sh
+++ aws_routemon.sh
@@ -3,21 +3,21 @@
 
 us-east-1-via-45() {
     setkey -c << EOF
-    spdupdate 10.142.24.0/21 10.142.32.0/21 any -P out ipsec esp/tunnel/10.142.31.7-54.240.217.164/require;
-    spdupdate 10.142.32.0/21 10.142.24.0/21 any -P in  ipsec esp/tunnel/54.240.217.164-10.142.31.7/require;
-EOF
-}
-
-us-east-1-via-41() {
-    setkey -c << EOF
     spdupdate 10.142.24.0/21 10.142.32.0/21 any -P out ipsec esp/tunnel/10.142.31.7-54.240.217.162/require;
     spdupdate 10.142.32.0/21 10.142.24.0/21 any -P in  ipsec esp/tunnel/54.240.217.162-10.142.31.7/require;
 EOF
 }
 
+us-east-1-via-41() {
+    setkey -c << EOF
+    spdupdate 10.142.24.0/21 10.142.32.0/21 any -P out ipsec esp/tunnel/10.142.31.7-54.240.217.164/require;
+    spdupdate 10.142.32.0/21 10.142.24.0/21 any -P in  ipsec esp/tunnel/54.240.217.164-10.142.31.7/require;
+EOF
+}
+
 declare -a actions
 actions[0]="10.142.32.0/21 169.254.255.45 169.254.255.41 us-east-1-via-45 us-east-1-via-41"
-actions[1]="10.142.88.0/21 169.254.88.45 169.254.88.41 foo bar"
+#actions[1]="10.142.88.0/21 169.254.88.45 169.254.88.41 foo bar"
 
 while :; do
     ip monitor | \
@@ -36,7 +36,7 @@ while :; do
             cmd2=${data[4]}
 
             if [ "$net" = "$network" ]; then
-                echo "route change, gw: $gw"
+                logger "aws-routemon: route change, new gw: $gw for $network"
 
                 if [ "$gw" = "$gw1" ]; then
                     $cmd1
